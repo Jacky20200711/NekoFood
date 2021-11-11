9e13a7fa827fb710c15562ebe7325710a6b43b0b
Jacky20200711 <fewer135+1@gmail.com>
2021-11-11 18:42:03 +0800

    建立專案

---
+++ NekoFood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NekoFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

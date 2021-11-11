75abd7c25ea626b6cc43167a888ecb6e6c0935f6
Jacky20200711 <fewer135+1@gmail.com>
2021-11-11 19:33:57 +0800

    安裝必要套件 & 設定NLog

--- NekoFood/Program.cs
+++ NekoFood/Program.cs
@@ -1,4 +1,9 @@
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// ³]©w NLog
+builder.Host.UseNLog();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,17 +14,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# NekoFood
+# 安裝套件  
+dotnet add package Microsoft.AspNetCore.Session --version 2.2.0  
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 6.0.0  
+dotnet add package Microsoft.EntityFrameworkCore.Tools --version 6.0.0  
+dotnet add package NLog --version 4.7.12  
+dotnet add package NLog.Web.AspNetCore --version 4.14.0  
